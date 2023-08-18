ae27b7c759b36c442fa543d9568f4d37a1acf5f1
Muhammed Gençay <gencaymuhammederol@gmail.com>
2023-08-18 12:42:38 +0300

    newww

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "steppers/SpinTable.h"
 
 void setup() {
-
+  Serial.begin(9600);
   init_servo_motor();
   init_start_button();
   init_spin_motor();
--- src/steppers/SpinTable.cpp
+++ src/steppers/SpinTable.cpp
@@ -1,13 +1,15 @@
 #include "SpinTable.h"
 #include "potansiyometer/SpeedPot.h"
 
+#define NUM_STEPS 1600
+
 AccelStepper spinMotor(1, SPIN_MOTOR_PUL, SPIN_MOTOR_DIR);
 
 
 void init_spin_motor()
 {
     //spinMotor.setEnablePin(SPIN_MOTOR_ENA);
-    spinMotor.setMaxSpeed(100);
+    spinMotor.setMaxSpeed(1000);
     spinMotor.setAcceleration(100);
     spinMotor.setSpeed(100);
     pinMode(SPIN_MOTOR_ENA, OUTPUT);
@@ -19,22 +21,25 @@ void spinCW()
 {
   delay(1000);
   digitalWrite(SPIN_MOTOR_ENA, LOW);
-  spinMotor.move(200);
+  spinMotor.move(NUM_STEPS);
 
-  while(spinMotor.currentPosition() != 200){
+  while(spinMotor.currentPosition() != NUM_STEPS){
 
-    /*int potValue = (int) readPotVaules();
+    int potValue = (int) readPotVaules();
 
     // Potansiyometre değerini hız aralığına dönüştürün
     // Örneğin, 0-1023 arasındaki analog değeri 1000-2000 adım/s arasındaki hıza dönüştürebilirsiniz
-    float speed = map(potValue, 0, 1023, 1000, 2000);*/
+    float speed = map(potValue, 0, 1023, 0,800);
+
 
     // Adım motorunun hızını güncelleyin
-    spinMotor.setSpeed(800);
+    spinMotor.setSpeed(speed);
+    //Serial.println(speed);
 
     // Adım motorunu belirtilen hızda hareket ettirin
     spinMotor.runSpeed();
   }
+  spinMotor.setCurrentPosition(0);
   delay(1000);
 
   return;
@@ -50,7 +55,7 @@ void spinCCW()
 
     // Potansiyometre değerini hız aralığına dönüştürün
     // Örneğin, 0-1023 arasındaki analog değeri 1000-2000 adım/s arasındaki hıza dönüştürebilirsiniz
-    float speed = map(potValue, 0, 1023, 1000, 2000);
+    float speed = map(potValue, 0, 1023, 0, 1000);
 
     // Adım motorunun hızını güncelleyin
     spinMotor.setSpeed(speed);
